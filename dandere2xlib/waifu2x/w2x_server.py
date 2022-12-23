96c6737ffe8565954504d9f8ef4049da6072f440
tyler <akai.katto@gmail.com>
2022-12-23 14:59:48 -0800

    added os detection for binary path

--- dandere2xlib/waifu2x/w2x_server.py
+++ dandere2xlib/waifu2x/w2x_server.py
@@ -32,7 +32,11 @@ class W2xServer(threading.Thread):
         self._receive_port = receive_port
         self._send_port = send_port
         self._waifu2x_location = Path(load_executable_paths_yaml()["w2x_vulkan_server"])
-        self._executable_location = self._waifu2x_location / "waifu2x-ncnn-vulkan.exe"
+
+        if dandere2xlib.utilities.dandere2x_utils.get_operating_system() == "linux":
+            self._executable_location = self._waifu2x_location / "waifu2x-ncnn-vulkan"
+        else:
+            self._executable_location = self._waifu2x_location / "waifu2x-ncnn-vulkan.exe"
 
         self._model_name = self.dandere2x_session.output_options["waifu2x_ncnn_vulkan"]["model_name"]
         self._noise_factor = self.dandere2x_session.noise_factor
